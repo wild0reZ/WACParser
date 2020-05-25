acfa0669dd1f9a09594f2aac7fb7bbe04240a716
wildzer0 <wildzer0@MacBook-Pro-di-wildzer0.local>
2020-05-25 18:47:54 +0200

    Modificato il file. DA SISTEMARE

--- test_parsing.py
+++ test_parsing.py
@@ -1,5 +1,7 @@
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def remove_unicode(row):
@@ -32,21 +34,48 @@ with open('output.txt', 'w') as out:
         out.write("%s\n" % line)
     out.close()
 
-date = []; time = []; sender = []; message = []
-with open('output.txt') as fp:
-    for row in fp.readlines():
-        current_word = complete.match(row)
-        if current_word is not None:
-            date.append(current_word.group(1))
-            time.append(current_word.group(2))
-            sender.append(current_word.group(3))
-            message.append(current_word.group(4).strip().replace('~', ' '))
-        else:
-            continue
-        #print('Still processing the chat. Wait!')
-    datetime = [dt[0] + ' ' + dt[1] for dt in zip(date, time)]
-    datetime = pd.to_datetime(datetime)
-    df = pd.DataFrame(zip(datetime, sender, message), columns=[
-                            'datetime', 'sender', 'message'])
-    df.to_csv('chat.csv', index=False, sep="~")
-    fp.close()
+def makeDf():
+    datetime = []
+    sender = []
+    message = []
+    with open('output.txt') as fp:
+        for row in fp.readlines():
+            current_word = complete.match(row)
+            if current_word is not None:
+                datetime.append(current_word.group(
+                    1) + ' ' + current_word.group(2))
+                sender.append(current_word.group(3))
+                message.append(current_word.group(4).strip().replace('~', ' '))
+            else:
+                continue
+            #print('Still processing the chat. Wait!')
+        datetime = pd.to_datetime(datetime)
+        df = pd.DataFrame(zip(datetime, sender, message), columns=[
+            'datetime', 'sender', 'message'])
+        df.to_csv('chat.csv', index=False, sep="~")
+        fp.close()
+        return df
+
+
+def generateGraph(csv_file):
+    data = pd.read_csv(csv_file, delimiter="~")
+    bool_series = pd.notnull(data['message'])
+    data = data[bool_series]
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    new_df = data.groupby('sender')['message'].count().to_frame()
+    new_df = new_df.rename(columns={'message': 'number_of_messages'})
+    new_df = new_df.reset_index()
+    tuples = new_df[['sender', 'number_of_messages']].apply(tuple)
+    a, b = tuples
+    c = []
+    for elem in a:
+        c.append(elem.replace(u'\xa0', ''))
+    fig, ax = plt.subplots()
+    ax.barh(c,b)
+    for index, value in enumerate(b):
+        ax.text(value, index, str(value))
+    ax.set_xlabel('Numero di Messaggi')
+    ax.set_ylabel('Amici')
+    plt.show()
+    
+generateGraph('chat.csv')
